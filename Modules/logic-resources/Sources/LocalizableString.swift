9613776630a11e72277910a08a6a90b3ed300da3
Michail Koumtzis <michail.koumtzis@netcompany.com>
2024-12-19 12:41:51 +0200

    document details issuer card

--- Modules/logic-resources/Sources/LocalizableString.swift
+++ Modules/logic-resources/Sources/LocalizableString.swift
@@ -325,6 +325,12 @@ public final class LocalizableString: LocalizableStringType {
       bundle.localizedString(forKey: "data_sharing_request")
     case .dataSharingTitle:
       bundle.localizedString(forKey: "data_sharing_title")
+    case .close:
+      bundle.localizedString(forKey: "close")
+    case .trustedRelyingParty:
+      bundle.localizedString(forKey: "trusted_relying_party")
+    case .trustedRelyingPartyDescription:
+      bundle.localizedString(forKey: "trusted_relying_party_description")
     }
   }
 
@@ -480,6 +486,9 @@ public extension LocalizableString {
     case details
     case dataSharingRequest
     case dataSharingTitle
+    case close
+    case trustedRelyingParty
+    case trustedRelyingPartyDescription
   }
 }
 
